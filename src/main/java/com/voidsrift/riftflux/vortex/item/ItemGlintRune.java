2669862711be9e7e784ef7033f9e920d9429900a
brandyn <sonicbrandyn@gmail.com>
2026-02-17 22:27:58 +0000

    Glint rune tooltip

--- src/main/java/com/voidsrift/riftflux/vortex/item/ItemGlintRune.java
+++ src/main/java/com/voidsrift/riftflux/vortex/item/ItemGlintRune.java
@@ -5,10 +5,13 @@ import cpw.mods.fml.relauncher.SideOnly;
 import java.util.List;
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.EnumRarity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.util.IIcon;
+import net.minecraft.util.StatCollector;
 
 public class ItemGlintRune extends Item {
    public IIcon[] icons = new IIcon[17];
@@ -46,5 +49,10 @@ public class ItemGlintRune extends Item {
 
    public EnumRarity getRarity(ItemStack p_77613_1_) {
       return EnumRarity.epic;
+   }
+
+   @Override
+   public void addInformation(ItemStack stack, EntityPlayer player, List list, boolean advanced) {
+      list.add(EnumChatFormatting.GRAY + StatCollector.translateToLocal("tooltip.riftflux.glintrune"));
    }
 }
